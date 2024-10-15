09b299310934cca38f44b7f6ea63843ee13398fa
saifi476 <isaifulislam.476@gmail.com>
2024-10-15 16:37:54 +0500

    Updated Privacy policy and tnc

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -14,18 +14,28 @@ const PrivacyPolicy = ({ open, onClose }) => {
       disableCross
     >
       <Heading>Privacy Policy</Heading>
-      <Title>This is title of privacy policy</Title>
-      <Para>This is para of privacy policy</Para>
-      <Title>This is title of privacy policy</Title>
+      <Title>Effective Date: October 15, 2024</Title>
+      <Para>At Gamebole, we respect your privacy and are committed to protecting it. This privacy policy outlines how we handle any personal information we may encounter when you visit our website.</Para>
+      <Title>1. No Collection of Personal Data</Title>
       <Para>
-        This is para a ois para of pis para of pis a ois para of pis para of pis
-        a ois para of pis para of pis a ois para of pis para of pis
+      We do not collect any personal information from visitors to our website. This means we do not ask for, store, or use your name, email address, phone number, or any other personal information. You are free to browse our website without providing any personal data.
       </Para>
-      <Para>This is para of privacy policy</Para>
-      <Para>This is para of privacy policy</Para>
+      <Title>2. Cookies and Tracking Technologies</Title>
+      <Para>Our website does not use cookies or any form of tracking technology to gather information about your browsing behavior.</Para>
+      <Title>3. Third-Party Links</Title>
+      <Para>Our website may contain links to third-party websites or services. Please note that once you leave our website, we are not responsible for the privacy practices of other websites. We encourage you to read the privacy policies of any external websites you visit..</Para>
+      <Title>4. Data Security</Title>
+      <Para>Since we do not collect any personal data, we do not store or process any user information. As such, there are no data security concerns related to personal information on our website.</Para>
+      <Title>5. Changes to This Policy</Title>
+      <Para>We may update this privacy policy from time to time. Any changes will be posted on this page, and we encourage you to review this policy periodically. Your continued use of our website following any updates will signify your acceptance of the changes.</Para>
+      <Title>6. Contact Information</Title>
+      <Para>If you have any questions or concerns about this privacy policy, please feel free to contact us at:</Para>
+      
       <List>
-        <ListItem>First item</ListItem>
-        <Para bold warning>
+        <ListItem>Gamebole</ListItem>
+        <ListItem>hr@gamebole.com</ListItem>
+        <ListItem>28 E, 2nd floor, main market, Lahore</ListItem>
+        {/* <Para bold warning>
           Banned Items
         </Para>
         <List>
@@ -34,8 +44,11 @@ const PrivacyPolicy = ({ open, onClose }) => {
           <ListItem>Item 3</ListItem>
         </List>
         <ListItem>Second item</ListItem>
-        <ListItem>Third item</ListItem>
+        <ListItem>Third item</ListItem> */}
       </List>
+      <Title>Compliance with Laws</Title>
+      <Para>Since we do not collect, store, or process any personal information, we are fully compliant with all relevant data protection laws, including GDPR and CCPA.:</Para>
+      
     </CustomDialog>
   );
 };
--- src/components/TermsAndConditions.jsx
+++ src/components/TermsAndConditions.jsx
@@ -14,18 +14,18 @@ const TermsAndConditions = ({ open, onClose }) => {
       disableCross
     >
       <Heading>Terms and Conditions</Heading>
-      <Title>This is Terms and Conditions</Title>
-      <Para>This is para of Terms and Conditions</Para>
-      <Title>This is Terms and Conditions</Title>
+      <Title>Effective Date: October 15, 2024</Title>
+      <Para>Welcome to Gamebole. By accessing and using our website, you agree to comply with and be bound by the following terms and conditions. If you do not agree with any part of these terms, please do not use our website.</Para>
+      <Title>1. Use of the Website</Title>
       <Para>
-        This is para a ois para of pis para of pis a ois para of pis para of pis
-        a ois para of pis para of pis a ois para of pis para of pis
+      The content of this website is for informational purposes only. By using our website, you agree that:
       </Para>
-      <Para>This is para of Terms and Conditions</Para>
-      <Para>This is para of Terms and Conditions</Para>
+      
       <List>
-        <ListItem>First item</ListItem>
-        <Para bold warning>
+        <ListItem>You will use the website solely for lawful purposes.</ListItem>
+        <ListItem>You will not use the website in any way that may impair its functionality or accessibility.</ListItem>
+        <ListItem>You will not attempt to hack, disrupt, or compromise the security or integrity of the website or its content.</ListItem>
+        {/* <Para bold warning>
           Banned Items
         </Para>
         <List>
@@ -34,8 +34,23 @@ const TermsAndConditions = ({ open, onClose }) => {
           <ListItem>Item 3</ListItem>
         </List>
         <ListItem>Second item</ListItem>
-        <ListItem>Third item</ListItem>
+        <ListItem>Third item</ListItem> */}
       </List>
+      <Title>2. Intellectual Property</Title>
+      <Para>All content on this website, including text, images, logos, graphics, and design, is the intellectual property of Gamebole unless otherwise stated. You may not reproduce, distribute, or use any content from this website without prior written permission from Gamebole.</Para>
+      <Title>3. Services Offered</Title>
+      <Para>Gamebole offers various services, which are described on our website. While we strive to ensure that all descriptions and details are accurate, we do not guarantee that the information is error-free or up-to-date. We reserve the right to modify or discontinue any service at any time without prior notice</Para>
+      <Title>4. Third-Party Links</Title>
+      <Para>Our website may contain links to third-party websites or services. These links are provided for your convenience and do not signify our endorsement of such websites. We are not responsible for the content or availability of linked sites, nor do we accept any liability for their practices.</Para>
+      <Title>5. Limitation of Liability</Title>
+      <Para>To the fullest extent permitted by law, [Your Company Name] will not be liable for any direct, indirect, incidental, or consequential damages arising out of your use of this website or any services provided through it. This includes, but is not limited to, loss of data, revenue, or profit, whether due to negligence or other causes.</Para>
+      <Title>6. Disclaimer of Warranties</Title>
+      <Para>We make no warranties or representations regarding the accuracy, reliability, or availability of our website or its content. The website is provided "as is," and Gamebole disclaims all warranties, express or implied, including, but not limited to, implied warranties of merchantability and fitness for a particular purpose.</Para>
+      <Title>7. Changes to Terms and Conditions</Title>
+      <Para>Gamebole reserves the right to update these terms and conditions at any time. Any changes will be posted on this page, and your continued use of the website after changes are made constitutes your acceptance of the revised terms.</Para>
+      <Title>8. Governing Law</Title>
+      <Para>These terms and conditions are governed by and construed in accordance with the laws of Pakistan. By using this website, you agree to submit to the jurisdiction of the courts located in Pakistan for any disputes arising out of your use of the website.</Para>
+      
     </CustomDialog>
   );
 };
